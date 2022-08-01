3ffe85ac3504be401047071a7a82980957c01c37
yiwangzhifeng <1442050398@qq.com>
2022-08-01 21:22:35 +0800

    save

---
+++ thread.h
@@ -0,0 +1,9 @@
+#pragma once
+class noncopyable{
+    protected:
+        noncopyable(){}
+        ~noncopyable(){}
+    private:
+        noncopyable(const noncopyable&);
+        const noncopyable& operator=(const noncopyable&);
+};
